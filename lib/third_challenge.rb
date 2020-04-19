e11543e20778cee13919953365a3054b0423a38c
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-19 20:01:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,8 +11,8 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -27,8 +27,8 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-         name: "Juliet"
-        age: "15"
+         name: "Juliet",
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
