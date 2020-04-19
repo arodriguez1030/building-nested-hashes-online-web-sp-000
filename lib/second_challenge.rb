b16103dba1eb3dc7dd78c01c72f59e97d8b53402
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-19 19:57:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
-      patriarch: {}, matriarch: {}, hero: {}, heroine_friends:[]
+      patriarch: {}, matriarch: {}, heroine: {}, heroine_friends:[]
       }}
 
   
